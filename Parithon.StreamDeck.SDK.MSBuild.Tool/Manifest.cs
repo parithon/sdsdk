db09ca2e9889409456c3234e9021c6ca9350bca8
Anthony Conrad <parithon@outlook.com>
2021-11-14 03:17:07 -0800

    Fixed MSBuild tool.

---
+++ Parithon.StreamDeck.SDK.MSBuild.Tool/Manifest.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Parithon.StreamDeck.SDK;
+
+internal class Manifest
+{
+  public Manifest(Assembly assembly)
+  {
+    this.Icon = assembly.GetCustomAttribute<AssemblyStreamDeckIconAttribute>()?.Icon ?? "Unknown";
+    this.OS = GetOS(assembly.GetCustomAttributes<AssemblyStreamDeckOSAttribute>());
+  }
+
+  private IEnumerable<dynamic> GetOS(IEnumerable<AssemblyStreamDeckOSAttribute> osattribs)
+  {
+    if (!osattribs.Any())
+    {
+      yield return new { Platform = "windows", MinimumVersion = "10" };
+      yield break;
+    }
+    foreach (var osattrib in osattribs)
+    {
+      yield return new { osattrib.Platform, osattrib.MinimumVersion };
+    }
+  }
+
+  public ICollection<dynamic> Actions { get; private set; } = new List<dynamic>();
+  public string Icon { get; private set; }
+  public IEnumerable<dynamic> OS { get; private set; } = new List<dynamic>();
+}
---
+++ Parithon.StreamDeck.SDK.MSBuild.Tool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Assembly buildAssembly = Assembly.LoadFrom(args[0]);
+Manifest manifest = new Manifest(buildAssembly);
+
+string manifestJSON = JsonSerializer.Serialize(manifest, new JsonSerializerOptions()
+{
+  DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+  WriteIndented = true
+});
+
+Console.WriteLine(manifestJSON);
+
+string? targetPath = Path.GetDirectoryName(args[0]);
+if (targetPath == null) return;
+
+File.WriteAllText(Path.Combine(targetPath, "manifest.json"), manifestJSON);
+
